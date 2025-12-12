43a01f35154b73a01f41dd93e4eda23b874f182d
MalaiarasuGRaj <govindarajmalaiarasu@gmail.com>
2025-12-12 22:06:14 +0530

    UI Updates

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { useState } from "react";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
 import { Analytics } from "@vercel/analytics/react"
 import ScrollToTop from "./components/layout/ScrollToTop";
 import { AuroraBackground } from "./components/layout/AuroraBackground";
@@ -28,14 +23,7 @@ const App = () => {
           <ScrollToTop />
           <AuroraBackground />
           <Analytics />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <AnimatedRoutes />
         </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
