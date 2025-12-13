fd6894239526b17192a177e2a6c3124b1ed8fca3
MalaiarasuGRaj <govindarajmalaiarasu@gmail.com>
2025-12-13 11:36:52 +0530

    Improved UX in Mobile View

--- src/components/layout/AuroraBackground.tsx
+++ src/components/layout/AuroraBackground.tsx
@@ -5,7 +5,7 @@ export const AuroraBackground = () => {
         <div className="fixed inset-0 z-[-1] overflow-hidden pointer-events-none bg-portfolio-black">
             {/* Top Left Blob */}
             <motion.div
-                className="absolute -top-[10%] -left-[10%] w-[50%] h-[50%] bg-portfolio-purple/20 rounded-full blur-[100px]"
+                className="absolute -top-[10%] -left-[10%] w-[50%] h-[50%] bg-portfolio-purple/20 rounded-full blur-[60px] md:blur-[100px] will-change-transform"
                 animate={{
                     x: [0, 50, 0],
                     y: [0, 30, 0],
@@ -18,9 +18,9 @@ export const AuroraBackground = () => {
                 }}
             />
 
-            {/* Top Right Blob */}
+            {/* Top Right Blob - Hidden on Mobile */}
             <motion.div
-                className="absolute top-[0%] right-[0%] w-[40%] h-[40%] bg-blue-500/20 rounded-full blur-[100px]"
+                className="absolute top-[0%] right-[0%] w-[40%] h-[40%] bg-blue-500/20 rounded-full blur-[100px] hidden md:block will-change-transform"
                 animate={{
                     x: [0, -30, 0],
                     y: [0, 50, 0],
@@ -33,9 +33,9 @@ export const AuroraBackground = () => {
                 }}
             />
 
-            {/* Bottom Left Blob */}
+            {/* Bottom Left Blob - Hidden on Mobile */}
             <motion.div
-                className="absolute bottom-[0%] -left-[10%] w-[40%] h-[40%] bg-portfolio-purple-light/20 rounded-full blur-[100px]"
+                className="absolute bottom-[0%] -left-[10%] w-[40%] h-[40%] bg-portfolio-purple-light/20 rounded-full blur-[100px] hidden md:block will-change-transform"
                 animate={{
                     x: [0, 40, 0],
                     y: [0, -40, 0],
@@ -50,7 +50,7 @@ export const AuroraBackground = () => {
 
             {/* Bottom Right Blob */}
             <motion.div
-                className="absolute -bottom-[10%] -right-[10%] w-[50%] h-[50%] bg-indigo-500/20 rounded-full blur-[100px]"
+                className="absolute -bottom-[10%] -right-[10%] w-[50%] h-[50%] bg-indigo-500/20 rounded-full blur-[60px] md:blur-[100px] will-change-transform"
                 animate={{
                     x: [0, -40, 0],
                     y: [0, -40, 0],
